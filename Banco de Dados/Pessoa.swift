c90720605273bdb74cf563fcf703a368ef963494
Faculdade Alfa <faculdadealfa@Mac-07.local>
2019-02-16 08:31:04 -0200

    init

---
+++ Banco de Dados/Pessoa.swift
@@ -0,0 +1,19 @@
+//
+//  Pessoa.swift
+//  Banco de Dados
+//
+//  Created by Faculdade Alfa on 16/02/2019.
+//  Copyright © 2019 Faculdade Alfa. All rights reserved.
+//
+
+import UIKit
+
+class Pessoa {
+    var nome: String
+    var cidade: String
+    
+    init(nome: String, cidade: String) {
+        self.nome = nome
+        self.cidade = cidade
+    }
+}
